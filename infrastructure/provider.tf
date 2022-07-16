0ee7b57dce339f97cf357016890aa7a10151c29e
Brenda <brenda_janu@icloud.com>
2022-07-16 20:29:28 -0300

    add glue ias and yaml deploy

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -2,7 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
 variable "account_number" {
   default = "485242421974"
+}
+
+terraform {
+  backend "s3" {
+    bucket = aws_s3_bucket.cognitivo-lake.id
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
