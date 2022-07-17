311c1d31c475b1a5c902d09e5b64cf8ad1de5847
Brenda <brenda_janu@icloud.com>
2022-07-17 14:02:20 -0300

    add role glue

--- infrastructure/iam_role.tf
+++ infrastructure/iam_role.tf
@@ -11,20 +11,6 @@ resource "aws_iam_role" "glue" {
       },
       "Effect": "Allow",
       "Sid": ""
-    },
-    {
-        "Action": [
-            "iam:PassRole"
-        ],
-        "Effect": "Allow",
-        "Resource": "arn:aws:iam::*:role/AWSGlueServiceRole*",
-        "Condition": {
-            "StringLike": {
-                "iam:PassedToService": [
-                    "glue.amazonaws.com"
-                ]
-            }
-        }
     }
   ]
 }
